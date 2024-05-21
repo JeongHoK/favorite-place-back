318fee0a82b35d43e70dda6bf5dfbd5799bdbea3
jeongho <wjdgh9531@gmail.com>
2024-05-21 16:39:24 +0900

    first commit

---
+++ src/main/java/com/favorite/place/FavoritePlaceApplication.java
@@ -0,0 +1,13 @@
+package com.favorite.place;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FavoritePlaceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FavoritePlaceApplication.class, args);
+	}
+
+}
