c49d2dcd82a09c55783a91c6b5bb43872c7c61b2
jeongho <wjdgh9531@gmail.com>
2024-05-28 15:51:48 +0900

    Add: Spring Security 추가, password 암호화 추가

--- src/main/java/com/favorite/place/controller/UserController.java
+++ src/main/java/com/favorite/place/controller/UserController.java
@@ -4,6 +4,7 @@ import com.favorite.place.dto.UserDTO;
 import com.favorite.place.entity.User;
 import com.favorite.place.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
@@ -13,17 +14,19 @@ public class UserController {
 
     private final UserService userService;
 
+    private final PasswordEncoder passwordEncoder;
+
     @Autowired
-    public UserController(UserService userService) {
+    public UserController(UserService userService, PasswordEncoder passwordEncoder) {
         this.userService = userService;
+        this.passwordEncoder = passwordEncoder;
     }
 
     @PostMapping("/user/join")
     public void joinUser(@RequestBody UserDTO userDTO) {
-
         User user = User.builder()
                             .name(userDTO.getName())
-                            .password(userDTO.getPassword())
+                            .password(passwordEncoder.encode(userDTO.getPassword()))
                             .phoneNumber(userDTO.getPhoneNumber())
                             .userRole(userDTO.getUserRole())
                             .email(userDTO.getEmail())
