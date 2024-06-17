d3789b648b2babfe39160cf7f2c1564b505f8fa3
jeongho <wjdgh9531@gmail.com>
2024-06-17 17:31:43 +0900

    Upd: 오타 수정

--- src/main/java/com/favorite/place/controller/AuthController.java
+++ src/main/java/com/favorite/place/controller/AuthController.java
@@ -20,9 +20,9 @@ public class AuthController {
     }
 
     @PostMapping("/signup")
-    public void signUp(@Valid @RequestBody SignUpRequest singupRequest) {
+    public void signUp(@Valid @RequestBody SignUpRequest signUpRequest) {
 
-        userService.signUp(singupRequest);
+        userService.signUp(signUpRequest);
     }
 
     @PostMapping("/login")
